771a95cb21cebc9876f3142bf6b05554c08e8215
ProgramRipper <programripper@foxmail.com>
2025-05-29 21:23:38 +0800

    :recycle: refactor: switch only

--- src/index.user.ts
+++ src/index.user.ts
@@ -13,39 +13,37 @@
       url.startsWith("//") ? "https:" + url : url,
     );
 
-    switch (method) {
-      case "GET":
-        if (
-          host === "api.live.bilibili.com" &&
-          pathname === "/xlive/app-blink/v1/live/GetWebLivePermission"
-        ) {
-          const getter = Object.getOwnPropertyDescriptor(
-            XMLHttpRequest.prototype,
-            "responseText",
-          )?.get;
+    switch ([method, host, pathname].toString()) {
+      case [
+        "GET",
+        "api.live.bilibili.com",
+        "/xlive/app-blink/v1/live/GetWebLivePermission",
+      ].toString():
+        const getter = Object.getOwnPropertyDescriptor(
+          XMLHttpRequest.prototype,
+          "responseText",
+        )?.get;
 
-          Object.defineProperty(this, "responseText", {
-            get() {
-              const resp = JSON.parse(getter?.call(this));
-              resp.data.allow_live = true;
-              return JSON.stringify(resp);
-            },
-          });
-        }
+        Object.defineProperty(this, "responseText", {
+          get() {
+            const resp = JSON.parse(getter?.call(this));
+            resp.data.allow_live = true;
+            return JSON.stringify(resp);
+          },
+        });
 
         break;
-      case "POST":
-        if (
-          host === "api.live.bilibili.com" &&
-          pathname === "/room/v1/Room/startLive"
-        ) {
-          this.send = (body: string) => {
-            const searchParams = new URLSearchParams(body);
+      case [
+        "POST",
+        "api.live.bilibili.com",
+        "/room/v1/Room/startLive",
+      ].toString():
+        this.send = (body: string) => {
+          const searchParams = new URLSearchParams(body);
 
-            searchParams.set("platform", "pc_link");
-            XMLHttpRequest.prototype.send.call(this, searchParams.toString());
-          };
-        }
+          searchParams.set("platform", "pc_link");
+          XMLHttpRequest.prototype.send.call(this, searchParams.toString());
+        };
 
         break;
     }
